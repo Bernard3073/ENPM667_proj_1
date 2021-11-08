b0c81a57755aaa110852a2fd7e0462c6867ea154
Bernard <Bernard@MSI.myfiosgateway.com>
2021-11-08 14:41:48 -0500

    obtain x_dot

--- WMR_simulation.m
+++ WMR_simulation.m
@@ -14,7 +14,7 @@ L_a = 0.1;
 theta_dot_r = 0;
 theta_dot_l = 0;
 % vector of wheel velocities
-niu = [w_r, w_l];  
+niu = [theta_dot_r; theta_dot_l];  
 % desired velocity
 v_d  = [0, 1.414];
 mat_A = [-sin(phi) cos(phi) -d 0 0;
@@ -28,15 +28,17 @@ mat_S = [c*(b*cos(phi) - d*sin(phi)) c*(b*cos(phi) + d*sin(phi));
 % sampling time
 h = 0.02;
 %********straight line path Jacobian ****
-A = -1;s
+A = -1;
 B = 1;
 C = 0;
-J_h_1 = 1/sqrt(A^2 + B^2) * [A; B; B*L_a*cos(phi) - A*L_a*sin(phi); 0; 0;];
+J_h_1 = 1/sqrt(A^2 + B^2) * [A, B, B*L_a*cos(phi) - A*L_a*sin(phi), 0, 0];
 J_h_2 = [r/2, r/2];
 % 2*2 matrix
 phi_func = [J_h_1 * mat_S; J_h_2];
 phi_func_diff = diff(phi_func);
 det_phi_func = r^2*(d + L_a)*(B*cos(phi) - A*sin(phi)) / 2*b*sqrt(A^2 + B^2); 
 % error between the desired velocity and the current velocity
-v = [v_d[0] - theta_dot_, v_d[1]];
-u = inv(phi_func) * (v - phi_func_diff * niu);
+v = [v_d(1) - theta_dot_r; v_d(2) - theta_dot_l];
+phi_func_inv = inv(phi_func);
+u = phi_func_inv * (v - phi_func_diff * niu);
+x_dot = [mat_S*niu; 0; 0] + [0 0;0 0;0 0;0 0;0 0;1 0;0 1]*niu;
