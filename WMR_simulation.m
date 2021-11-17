8c4f4ce438603920f828902e1767be7908f39ef5
Bernard <Bernard@MSI.myfiosgateway.com>
2021-11-17 15:07:33 -0500

    add some parmeters

--- WMR_simulation.m
+++ WMR_simulation.m
@@ -59,6 +59,8 @@ h = 0.02;
 A = -1;
 B = 1;
 C = 0;
+x_c = 0
+y_c = 0
 J_h_1 = (1/sqrt(A^2 + B^2)).*[A, B, (B*L_a*cos(phi) - A*L_a*sin(phi)), 0, 0];
 J_h_2 = [r/2, r/2];
 % 2*2 matrix
@@ -72,6 +74,25 @@ phi_func_inv = inv(phi_func);
 u = phi_func_inv * (v - phi_func_diff * niu);
 x_dot = [mat_S*niu; 0; 0] + [0 0;0 0;0 0;0 0;0 0;1 0;0 1]*niu;
 
+% the coordinates of the point P_l 
+x_l = x_c + L_a * cos(phi)
+y_l = y_c + L_a * sin(phi)
+h1= (-x_l + y_l)/2
+h2 = r*(v(1) + v(2))/2
+
+% position model
+y_k = [h1, h2]
+C_r = [1,  0, 0, 0]
+C_a = [C_r, 0]
+y_k_hat = C_a * 
+Q_x_r = 10^(-5)*eye(4)
+Q_p_k = 10^(-4)
+R_k = 10^(-3)
+P_0 = [Q_x_r  0; ]
+% Kalman gain
+K_k = [0.1407, 0.3018, 0.2931, 0.2931, 0.2931].T
+phi_1 = [1, h; 0, 1]
+gamma_1 = [h^2/2; h]
 % plot a straight line
     b0 = 3;
     b1 = 4;
